9f0dbfea134a443518a383b1561e8e426a99f7fb
Kris Ames <krisean@uvic.ca>
2025-12-29 23:28:03 -0800

    Delete functions directory

--- functions/src/games/topComment/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Top Comment Game Exports
- */
-
-export * from "./types";
-export * from "./sharedLogic";
-export * from "./TopCommentEventGame";
-export * from "./TopCommentSoloGame";
-
--- functions/uipatch.js
+++
@@ -1,3 +0,0 @@
-const { Firestore } = require('firebase-admin/firestore');
-const { initializeApp, applicationDefault } = require('firebase-admin/app');
-const db = new Firestore({ projectId: 'dummy', credentials: null });
