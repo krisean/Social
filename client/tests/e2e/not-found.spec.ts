b6d98e4f4a1e74c44549ff81a6516ded8f6059e2
Kris Ames <krisean@uvic.ca>
2025-12-29 23:28:22 -0800

    Delete client directory

--- client/tests/e2e/not-found.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("unknown routes render the not found page", async ({ page }) => {
-  await page.goto("/this-route-does-not-exist");
-
-  await expect(page.getByRole("heading", { name: /Page not found/i })).toBeVisible();
-  await expect(page.getByRole("link", { name: /Return home/i })).toBeVisible();
-});
